6801b8270e24fb6b81fbebac4a89b840e03c21af
rameshbabuvfx <rameshkanna95@gmail.com>
2021-10-10 16:59:23 +0530

    Add main review window

--- reviewMain.py
+++ reviewMain.py
@@ -15,7 +15,64 @@ class ReviewWindow(reviewMainUI.Ui_Form, QWidget):
         self.screen_grab_pushButton.clicked.connect(self.review_paint_tool)
 
     def review_paint_tool(self):
-        smartReview.main()
+        ScreenShot()
+
+
+
+class ScreenShot(QWidget):
+    def __init__(self):
+        super(ScreenShot, self).__init__()
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
+        self.setStyleSheet('''background-color:white; ''')
+        self.setWindowOpacity(0.7)
+        self.showMaximized()
+        self.setCursor(Qt.CrossCursor)
+        self.bmask = QBitmap(self.geometry().size())
+        self.bmask.fill(Qt.black)
+        self.mask = self.bmask.copy()
+        self.isDrawing = False
+        self.startPoint = QPoint()
+        self.endPoint = QPoint()
+        self.show()
+
+    def paintEvent(self, event):
+        if self.isDrawing:
+            self.mask = self.bmask.copy()
+            mask_painter = QPainter(self.mask)
+            pen = QPen()
+            pen.setStyle(Qt.NoPen)
+            mask_painter.setPen(pen)
+            brush = QBrush(Qt.white)
+            mask_painter.setBrush(brush)
+            mask_painter.drawRect(QRect(self.startPoint, self.endPoint))
+            self.setMask(QBitmap(self.mask))
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.startPoint = event.pos()
+            self.endPoint = self.startPoint
+            self.isDrawing = True
+
+    def mouseMoveEvent(self, event):
+        if self.isDrawing:
+            self.endPoint = event.pos()
+            self.update()
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.endPoint = event.pos()
+
+            main_window_id = QApplication.desktop().winId()
+            long_win_id = main_window_id
+            screenshot = QPixmap.grabWindow(long_win_id)
+            rect = QRect(self.startPoint, self.endPoint)
+            output_region = screenshot.copy(rect)
+            temp_path = r"D:\PythonProjects\NukePython\smart_review_tool\temp.jpg"
+            output_region.save(temp_path, format='JPG', quality=100)
+            saved_image = (QPixmap(temp_path).scaled(1580, 1020, Qt.KeepAspectRatio))
+            self.resize(saved_image.width(), saved_image.height())
+            self.close()
+            smartReview.main()
 
 
 if __name__ == '__main__':
